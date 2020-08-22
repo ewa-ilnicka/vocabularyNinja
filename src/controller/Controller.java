cc16af9e31f5c04f4e5d7e5cc99ba23e4413a221
ewa-ilnicka <ewa.h.ilnicka@gmail.com>
2020-08-22 16:51:17 +0200

    Added Item and Dictionary to Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,20 +2,37 @@ package controller;
 
 import view.GUI;
 
+import model.Dictionary;
+import model.Item;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import static model.Config.CHECK;
 import static model.Config.NEW_GAME;
 import static model.Config.OPEN;
 
-public class Controller {
+public class Controller implements ActionListener {
 
     private GUI gui;
+    private Dictionary dictionary;
+    private int currentItemNumber;
+    private Item randomItem;
+    private List<Item> copyOfList = new ArrayList<>();
 
     {
         gui = new GUI();
+        dictionary = new Dictionary();
     }
 
     public void run() {
         gui.createAndShowWindow();
+        gui.addListeners(this);
     }
 
     @Override
@@ -31,5 +48,6 @@ public class Controller {
                 handleOpenEvent();
         }
     }
+
 
 }
